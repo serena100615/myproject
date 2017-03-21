0414aaa0a53cba0d1dbfe31998142215fe001167
Serena <serena100615@gmail.com>
2017-03-21 16:29:51 +0800

    Fields,Car id

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -1,21 +1,18 @@
 package oo.abstraction;
 
 public class Car {
-	  String brand;
-	  int cc;
-	  String name;
-	  String type;
-	  int status;
-	  
-	  
-	  public Car (String brand, int cc, String name,String type ){
-		  this.brand = brand;
-		  this.cc = cc;
-		  this.name = name;
-		  this.type = type;
-	  }
+	int id;
+	String brand;
+	int cc;
+	String name;
+	String type;
+	int status;
 
-	
-	
-	 }
+	public Car(String brand, int cc, String name, String type) {
+		this.brand = brand;
+		this.cc = cc;
+		this.name = name;
+		this.type = type;
+	}
 
+}
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,11 +3,34 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car car1 = new Car("toyota",1800,"aa","sedan");
+		Car car1 = new Car("toyota", 1800, "altis", "sedan");
+		Car car2 = new Car("honda", 2000, "accord", "sedan");
+		Car car3 = new Car("toyota", 2000, "wish", "sedan");
+
+		int[] n = new int[5];
+		System.out.println(n[2]);
 		
-		Car car2 = new Car("honda",2000,"accord","sedan");
+		Car[] cars = new Car[3];
+		System.out.println(cars[0]);
+		cars[0] = new Car("toyota", 1800, "altis", "sedan");
+		cars[1] = new Car("honda", 2000, "accord", "sedan");
+		cars[2] = new Car("toyota", 2000, "wish", "sedan");
+		cars[0].id = 301;
+		cars[1].id = 400;
+		cars[2].id = 403;
 		
-		Car car3 = new Car("toyota",2000,"wish","sedan");
+		System.out.println(cars[0].name);
+		int i = 0;
+		for (i = 0; i < 3; i++) {
+			System.out.println(cars[i].name);
+		}
+		System.out.println(i);
+		
+
+	class AA {
+
 	}
 
+	AA a = new AA();
+}
 }
