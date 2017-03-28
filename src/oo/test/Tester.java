05ff362389c484d59e14e295f847fd698ecab72b
Serena <serena100615@gmail.com>
2017-03-28 16:01:27 +0800

    ArrayList

--- src/oo/test/Tester.java
+++ src/oo/test/Tester.java
@@ -1,13 +1,27 @@
 package oo.test;
 
+import java.util.ArrayList;
+
 import oo.abstraction.Car;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		Car c = new Car("brand",2000,"name","type");
-		
-
+		Car c = new Car("brand", 2000, "name", "type");
+		ArrayList<String> a = new ArrayList();
+ 	 // String[] numbers= {"331","821","886"};
+		a.add("331");
+		a.add("821");
+     // a.add(123);
+		a.add("886");
+   	 // a.add(true);
+		System.out.println(a.size());
+		a.add("554");
+		System.out.println(a.size());
+		a.set(2, "885");
+		System.out.println(a);
+		String data = a.get(3);
+	 // int n = (int)a.get(4);
 	}
 
 }
