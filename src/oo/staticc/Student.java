c53ad43e2075be92be94af9c7da84f79c6a9e77b
Serena <serena100615@gmail.com>
2017-04-11 15:14:34 +0800

    static

---
+++ src/oo/staticc/Student.java
@@ -0,0 +1,32 @@
+package oo.staticc;
+
+public class Student {
+
+	int english;
+	int math;
+	int chinese;
+	static int pass = 60;
+
+	public Student(int english, int math, int chinese) {
+		super();
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+	}
+
+	/*
+	 * public void print(){
+	 * System.out.println(english+"\t"+math+"\t"+chinese+"\t"); }
+	 */
+	public void print() {
+		System.out.println(getScore(english) + "\t" + getScore(math) + "\t" + getScore(chinese));
+	}
+
+	private String getScore(int score) {
+		if (score < pass) {
+			return String.valueOf(score) + "*";
+		} else {
+			return String.valueOf(score);
+		}
+	}
+}
---
+++ src/oo/staticc/Tester.java
@@ -0,0 +1,14 @@
+package oo.staticc;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		System.out.println(Student.pass);
+		Student stu1 = new Student(58, 98, 66);
+		Student stu2 = new Student(67, 56, 77);
+		stu1.pass = 70;
+		stu1.print();
+		stu2.print();
+	}
+
+}
