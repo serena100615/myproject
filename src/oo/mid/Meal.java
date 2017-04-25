3ae1c076edb52d83ec9019801e49781c3deb42cd
Serena <serena100615@gmail.com>
2017-04-25 14:26:34 +0800

    Middle Exam

---
+++ src/oo/mid/Meal.java
@@ -0,0 +1,17 @@
+package oo.mid;
+
+public class Meal {
+	String name;
+	String size;
+	int price;
+
+	public Meal(String name, String size, int price) {
+		this.name = name;
+		this.size = size;
+		this.price = price;
+	}
+
+	public void print() {
+		System.out.println();
+	}
+}
---
+++ src/oo/mid/Ticket.java
@@ -0,0 +1,37 @@
+package oo.mid;
+
+public class Ticket {
+	int id;
+	String name;
+	float price;
+
+	public Ticket(int id, String name, float price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public void setId(int i) {
+
+	}
+
+	public void getId(int i) {
+
+	}
+
+	public void setName(String n) {
+
+	}
+
+	public void getName(String n) {
+
+	}
+
+	public void setPrice(float p) {
+
+	}
+
+	public void getPrice(float p) {
+
+	}
+}
