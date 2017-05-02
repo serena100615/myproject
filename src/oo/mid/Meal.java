39a3017e4ed94d0652edabddb32229849e6e603d
Serena <serena100615@gmail.com>
2017-05-02 14:13:15 +0800

    add

--- src/oo/mid/Meal.java
+++ src/oo/mid/Meal.java
@@ -2,18 +2,18 @@ package oo.mid;
 
 public class Meal {
 	String name;
-	String size;
+	String snack;
 	int price;
 
-	public Meal(String name, String size, int price) {
+	public Meal(String name, int price,String snack) {
 		this.name = name;
-		this.size = size;
 		this.price = price;
+		this.snack=snack;
 	}
 
-	public void print() {
+	public int discount() {
 		System.out.println();
-		
-		
+		int dis =(int)(price*0.9f);
+		return dis; 		
 	}
 }
