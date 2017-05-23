2d9bd5303037b4c7b4698b48a6ef6ab78940b362
user <user@es302n12>
2017-05-23 16:08:22 +0800

    try & catch

---
+++ src/oo/io/SumScore.java
@@ -0,0 +1,28 @@
+package oo.io;
+
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class SumScore {
+
+	public static void main(String[] args) {
+		// FileOutputStream fos = new FileOutputStream("data.txt");
+		int sum = 0;
+		Scanner scanner = new Scanner(System.in);
+		for (int i = 0; i < 3; i++) {
+			System.out.print("請輸入成績:");
+			String data = scanner.nextLine();
+			try {
+				int score = Integer.parseInt(data);
+				sum = sum + score;
+			} catch (NumberFormatException e) {
+				System.out.println("來亂的?");
+				i--;
+
+			}
+		}
+		System.out.println(sum);
+
+	}
+
+}
