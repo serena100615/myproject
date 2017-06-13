1f4498284738abe433f6eeb79a95068de8345ac5
Serena <serena100615@gmail.com>
2017-06-13 16:21:34 +0800

    public Member(String name)

--- src/oo/test/Member.java
+++ src/oo/test/Member.java
@@ -1,13 +1,18 @@
 package oo.test;
 
 public class Member {
-	String nmae;
-	int age;
+	String name;
+	private int age;
 	String id;
 	
-	public Member(){
+	public Member(String name){
 		System.out.println("member constructor");
+		this.name = name;
 	}
+	public Member(){
+		
+	}
+	
 	 void setAge(int age){
 		if(age<0){
 			age =0;
